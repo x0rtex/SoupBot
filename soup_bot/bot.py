1a01eeaf9710f0ca4de981657b187ab05c673b4d
x0rtex <39656613+x0rtex@users.noreply.github.com>
2022-09-30 23:00:18 +0100

    surgically removed useless code and it still works

--- soup_bot/bot.py
+++ soup_bot/bot.py
@@ -13,26 +13,6 @@ import psutil
 import random
 
 
-class MyNavButton(nav.NavButton):
-    # This is how you can create your own navigator button
-    # The extension also comes with the following nav buttons built-in:
-    #
-    # FirstButton - Goes to the first page
-    # PrevButton - Goes to previous page
-    # IndicatorButton - Indicates current page number
-    # StopButton - Stops the navigator session and disables all buttons
-    # NextButton - Goes to next page
-    # LastButton - Goes to the last page
-
-    async def callback(self, ctx: miru.Context) -> None:
-        await ctx.respond("You clicked me!", flags=hikari.MessageFlag.EPHEMERAL)
-
-    async def before_page_change(self) -> None:
-        # This function is called before the new page is sent by
-        # NavigatorView.send_page()
-        self.label = f"Page: {self.view.current_page+1}"
-
-
 bot = lightbulb.BotApp(
     os.environ["TOKEN"],
     default_enabled_guilds=int(os.environ["DEFAULT_GUILD_ID"]),
@@ -49,33 +29,6 @@ bot = lightbulb.BotApp(
 )
 
 miru.load(bot)
-
-
-@bot.listen()
-async def navigator(event: hikari.GuildMessageCreateEvent) -> None:
-
-    # Do not process messages from bots or empty messages
-    if event.is_bot or not event.content:
-        return
-
-    if event.content.startswith("mirunav"):
-        embed = hikari.Embed(title="I'm the second page!", description="Also an embed!")
-        pages = ["I'm the first page!", embed, "I'm the last page!"]
-        # Define our navigator and pass in our list of pages
-        miru_navigator = nav.NavigatorView(pages=pages)
-        # You may also pass an interaction object to this function
-        await miru_navigator.send(event.channel_id)
-
-    elif event.content.startswith("mirucustom"):
-        embed = hikari.Embed(title="I'm the second page!", description="Also an embed!")
-        pages = ["I'm a customized navigator!", embed, "I'm the last page!"]
-        # Define our custom buttons for this navigator
-        # All navigator buttons MUST subclass NavButton
-        buttons = [nav.PrevButton(), nav.StopButton(), nav.NextButton(), MyNavButton(label="Page: 1", row=1)]
-        # Pass our list of NavButton to the navigator
-        miru_navigator = nav.NavigatorView(pages=pages, buttons=buttons)
-
-        await miru_navigator.send(event.channel_id)
 
 
 @bot.command()
@@ -250,6 +203,9 @@ async def cmd_flow(ctx: lightbulb.Context) -> None:
 @lightbulb.command("album", "Upload multiple images/gifs are they will be stored in a compact album.")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def cmd_album(ctx: lightbulb.Context) -> None:
+    print(f"\nctx.resolved.attachments: {ctx.resolved.attachments}")
+    print(f"\nctx.resolved.attachments.values(): {ctx.resolved.attachments.values()}")
+    print(f"\n[i for i in ctx.resolved.attachments.values()]: {[i for i in ctx.resolved.attachments.values()]}")
     attachments = [i for i in ctx.resolved.attachments.values()]
 
     attachment_dict = {}
